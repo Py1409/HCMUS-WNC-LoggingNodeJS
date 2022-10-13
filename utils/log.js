667b80b21322ff9d15f46848839d94bb404d63ce
Py1409 <py.ltk1998@gmail.com>
2022-10-13 14:47:39 +0700

    logs rotate

--- utils/log.js
+++ utils/log.js
@@ -1,20 +1,37 @@
 import {createLogger, transports, format} from 'winston';
+import  'winston-daily-rotate-file';
+
+const transport = new transports.DailyRotateFile({
+    filename: 'logs/%DATE%.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
+  });
+
+// const log = createLogger({
+// transports:
+//     new transports.File({
+//     filename: `logs/${new Date().toJSON().slice(0,10).replace(/-/g,'-')}.log`,
+//     format:format.combine(
+//         format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+//         format.align(),
+//         format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+//     )}),
+// });
 
 const log = createLogger({
-transports:
-    new transports.File({
-    filename: `logs/${new Date().toJSON().slice(0,10).replace(/-/g,'-')}.log`,
-    format:format.combine(
-        format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-        format.align(),
-        format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
-    )}),
-});
+    transports: [
+      transport
+    ]
+  });
+
+
 
 export const logError = (err, req, res) =>{
-    log.error(`${err.status || 500} - ${res.statusMessage} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip} - req.body: ${JSON.stringify(req.body)}`);
+    log.error(`${new Date().toJSON()} - ${err.status || 500} - ${res.statusMessage} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip} - req.body: ${JSON.stringify(req.body)}`);
 }
 
 export const logInfo = (req, res) =>{
-    log.info(`${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip} - req.body: ${JSON.stringify(req.body)}`);
+    log.info(`${new Date().toJSON()} - ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip} - req.body: ${JSON.stringify(req.body)}`);
 }
